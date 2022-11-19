4de734f6b9538afb2cad802333d241d6ddc57e67
Pierre Ballif <pierre.ballif@tum.de>
2022-11-19 19:35:09 +0100

    Communicate with blockchain

--- client/main.js
+++ client/main.js
@@ -1,21 +1,36 @@
-const INDEXER_URL = "http://localhost:8980";
+const INDEXER_URL = "http://131.159.214.36:8980";
 
-function getUserData(){
+async function getUserData(){
 	const request = new XMLHttpRequest();
-	request.onreadystatechange = () => {
-		if(request.readyState === XMLHttpRequest.DONE) {
-			if(request.status < 300) {
-				console.log(request.responseText);
-			} else {
-				console.warn("ERROR");
+	return new Promise((resolve, reject) => {	
+		request.onreadystatechange = () => {
+			if(request.readyState === XMLHttpRequest.DONE) {
+				if(request.status < 300) resolve(request.responseText);
+				else reject(request);
 			}
 		}
-	}
-	request.open("GET", INDEXER_URL + "/v2/assets");
-	request.send();
-	return JSON.parse(request.responseText).assets;
+		request.open("GET", INDEXER_URL + "/v2/assets");
+		request.send();
+	});
 }
 
-function reloadUserData(){
-	const data = getUserData();
+async function reloadUserData(){
+	const data = await getUserData()
+	.then(data_string => {
+		const data = JSON.parse(data_string);
+		const assets = data.assets;
+		let assets_by_user = {};
+		for(let asset of assets){
+			if(assets_by_user[asset.params.manager])
+				assets_by_user[asset.params.manager].push(asset.params);
+			else
+				assets_by_user[asset.params.manager] = [ asset.params ];
+		}
+		for(let user in assets_by_users){
+			for(let asset of assets_by_users[user]){
+
+			}
+		}
+	});
+	return data;
 }
