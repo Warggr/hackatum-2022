ac3a349bffb60dd2f53191728f6a1831bed97f4c
Pierre Ballif <pierre.ballif@tum.de>
2022-11-19 18:51:43 +0100

    Move everything into client/ folder

--- client/main.js
+++ client/main.js
@@ -1,7 +1,6 @@
 const INDEXER_URL = "http://localhost:8980";
 
 function getUserData(){
-	/*
 	const request = new XMLHttpRequest();
 	request.onreadystatechange = () => {
 		if(request.readyState === XMLHttpRequest.DONE) {
@@ -15,13 +14,6 @@ function getUserData(){
 	request.open("GET", INDEXER_URL + "/v2/assets");
 	request.send();
 	return JSON.parse(request.responseText).assets;
-	*/
-	const MOCK_DATA = [
-		{ "type": "certificate", "owner": "Mock User", "name": "Bachelor's Degree", "signed-by": "TUM", "text": "Lorem Ipsum" },
-		{ "type": "certificate", "owner": "Mock User", "name": "Master's Degree", "signed-by": "TUM", "text": "Lorem Ipsum" },
-		{ "type": "document", "owner": "Mock User", "name": "CV", "signed-by": "Mock User", "text": "Lorem Ipsum" }
-	];
-	return MOCK_DATA;
 }
 
 function reloadUserData(){
