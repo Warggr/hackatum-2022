ebd53f2033665f85c5efe1a28675a29d2a539ab5
Pierre Ballif <pierre.ballif@tum.de>
2022-11-20 01:00:41 +0100

    Serve static files

--- server/server.py
+++ server/server.py
@@ -1,24 +1,32 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, send_file
 from create_certificate import create_certificate
 from create_acc import create_acc
 import os.path
 
-app = Flask(__name__, static_folder='user_content')
+UPLOAD_DIR = os.path.join(os.path.dirname(__file__), '..', 'content')
+WEB_FILES = os.path.join(os.path.dirname(__file__), '..', '..', 'client')
+
+print(UPLOAD_DIR, WEB_FILES)
+
+app = Flask(__name__, static_folder='content')
 
 user = None
 
 def upload_to_server(file):
-	file.save(os.path.join('user_content', file.filename))
-	return "http://localhost:5000/static/" + file.filename
+	file.save(os.path.join(UPLOAD_DIR, file.filename))
+	return "http://localhost:5000/content/" + file.filename
 
 @app.route('/upload', methods=['POST'])
 def create():
 	file = request.files['certificate']
 	filename = file.filename
 	file_url = upload_to_server(file)
-	print(user['pk'])
 	create_certificate(user, file.read(), file_url)
 	return Response('Created', status=201)
+
+@app.route('/web/<path:filepath>')
+def send_static(filepath):
+	return send_file(os.path.join(WEB_FILES, filepath))
 
 if __name__ == "__main__":
 	user = create_acc()
